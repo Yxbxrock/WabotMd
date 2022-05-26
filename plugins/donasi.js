b0f8bbe4ed3f1f8d927318335c9de985258960a4
Yxbxrock <102041995+Yxbxrock@users.noreply.github.com>
2022-05-26 08:34:49 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,10 +1,10 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ • Indosat [085713964963]
-│ • Gopay [085713964963]
+│ • Telkomsel [082365089294]
+│ • Dana [082365089294]
 ╰────
 ╭─「 Hubungi 」
-│ > Ingin donasi? Wa.me/6281390658325
+│ > Ingin donasi? Wa.me/6282365089294
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
